ba3728ae8a0838ab0c556970fcd367dc2768bb0a
elena25466551 <elenacastrodm76@gmail.com>
2023-10-09 12:12:57 -0300

    segundo swarm seminario

---
+++ swarm/server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port = 80;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Server 1</h1>");
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(
+        `Servidor escuchando en el puerto ${port}. Ir a http://localhost:${port}`);
+});
